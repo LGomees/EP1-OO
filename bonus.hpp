5004b2823214154002e3099dfa0baaea4e7b8823
Lucas <l.gomees_@hotmail.com>
2017-04-06 21:46:35 -0300

    Adicionando Headers e Makefile

---
+++ bonus.hpp
@@ -0,0 +1,19 @@
+#ifndef BONUS_HPP
+#define BONUS_HPP
+
+#include "elementosJogo.hpp"
+
+class Bonus : public ElementosJogo {
+
+   private:
+	int pontos;
+
+   public:
+	Pontos();
+	~Pontos();
+	int getPontos();
+	void setPontos(int pontos);
+
+};
+
+#endif
